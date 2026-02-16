40953c2841d390dfee2dde356a396c351f82ac53
louisexpc <louis60201@gmail.com>
2026-02-16 18:45:00 +0800

    remove unused package

--- shared_types.py
+++ shared_types.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 from datetime import datetime, timedelta, timezone
-from typing import Any, ClassVar, Literal, Sequence, Tuple
+from typing import Literal, Sequence, Tuple
 
 from zoneinfo import ZoneInfo
 
